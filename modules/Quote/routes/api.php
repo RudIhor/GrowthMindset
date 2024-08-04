e0dfad7ed9f3c1c466b80bfd1211cc0388aa1ea1
Ihor Rud <ihor.rud@onix-systems.com>
2024-08-04 14:58:26 +0200

    feat(applications): implement getting random quote from category with translation

--- modules/Quote/routes/api.php
+++ modules/Quote/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Quote\app\Http\Controllers\Api\QuoteController;
 
-Route::middleware(['auth:api', 'role:admin'])->group(function() {
-    Route::apiResource('quotes', QuoteController::class);
+Route::middleware(['auth:api'])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
+        Route::apiResource('quotes', QuoteController::class);
+    });
+});
+Route::middleware(['auth:application'])->group(function () {
+    Route::get('/quotes/category/{category}', [QuoteController::class, 'getRandomQuoteFromCategoryWithTranslation'])->name(
+        'quotes.category-quote-with-translation'
+    );
 });
